63e651df3b32870942077f5b62e4a414b77c66ba
spicyGrape <li.private0@gmail.com>
2023-04-03 19:59:34 +0800

    Update dataProcesser.py

--- dataProcesser.py
+++ dataProcesser.py
@@ -1,17 +1,19 @@
-def SUCCESSIONAL_DIFFERENCE():
+def SUCCESSIONAL_DIFFERENCE(variable_num,variable_list):
     sum = 0
-    variable_list=[]
-    variable_num=int(input('The number of variables is:'))
-    i = 1
-    while i <= variable_num:
-        variable_list.append(float(input("Input the variables:")))
-        i += 1
+    
     for i in range(0,variable_num//2):
         sum -= variable_list[i]
         sum +=variable_list[variable_num-1-i]
-    print(sum)#test
     sum /= (variable_num//2)
-    print(variable_num//2)#test
     sum /= (variable_num//2)
     return sum
-print(SUCCESSIONAL_DIFFERENCE())
+def GET_VARIABLE_LIST():
+    variable_num=int(input('Type in the number of variables: '))
+    variable_list = []
+    for i in range(variable_num):
+        variable_list.append(float(input("Input the variables: ")))
+        i += 1
+    return variable_num,variable_list
+#SUCCESSIONAL_DIFFERENCE(GET_VARIABLE_LIST())
+a,b=GET_VARIABLE_LIST()
+print(SUCCESSIONAL_DIFFERENCE(a,b))
