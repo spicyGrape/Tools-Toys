96a6482d008890bd214ef0b89dc5cd1d0ce313ad
WenzeJin <andyjibo@outlook.com>
2023-04-03 20:42:02 +0800

    more "Pythonic" implement

--- dataProcesser.py
+++ dataProcesser.py
@@ -1,19 +1,20 @@
-def SUCCESSIONAL_DIFFERENCE(variable_num,variable_list):
+def SUCCESSIONAL_DIFFERENCE(variable_list:list):
+    variable_num = len(variable_list)
     sum = 0
-    
     for i in range(0,variable_num//2):
         sum -= variable_list[i]
         sum +=variable_list[variable_num-1-i]
     sum /= (variable_num//2)
     sum /= (variable_num//2)
     return sum
-def GET_VARIABLE_LIST():
-    variable_num=int(input('Type in the number of variables: '))
+
+def GET_VARIABLE_LIST() -> list:
     variable_list = []
-    for i in range(variable_num):
-        variable_list.append(float(input("Input the variables: ")))
-        i += 1
-    return variable_num,variable_list
+    variable_list = eval(input("Input variables, use a comma \',\' between values:"))
+    
+    return variable_list
+
 #SUCCESSIONAL_DIFFERENCE(GET_VARIABLE_LIST())
-a,b=GET_VARIABLE_LIST()
-print(SUCCESSIONAL_DIFFERENCE(a,b))
+
+variables = GET_VARIABLE_LIST()
+print(SUCCESSIONAL_DIFFERENCE(variables))
