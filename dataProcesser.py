3da38e7bc0377e46498dd458eec8718dde3b1b2c
spicyGrape <li.private0@gmail.com>
2023-04-03 20:48:05 +0800

    some change

--- dataProcesser.py
+++ dataProcesser.py
@@ -28,4 +28,20 @@ def TRANSFORM_TO_DELTA(variable_list):
     variable_list[0]=variable_list[0]//2
     return variable_list
 
+def LIST_AVERAGE(variable_list):
+    sum = 0
+    for i in range(1,variable_list[0]+1):
+        sum += variable_list[i]
+    average = sum / variable_list[0]
+    return average
+
+def UNCERTAINTY(variable_list,ub):
+    delta_list = TRANSFORM_TO_DELTA(variable_list)
+    average = LIST_AVERAGE(variable_list)
+    delta_list = OPERATE_VARIABLE_LIST(delta_list,lambda x:(x-average)**2)
+    ua=(SIGMA(delta_list)/delta_list[0])**0.5
+    u = (ua**2+ub**2)**0.5
+    return u
+
+
 print(SUCCESSIONAL_DIFFERENCE(GET_VARIABLE_LIST()))
