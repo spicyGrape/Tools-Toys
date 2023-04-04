751330c7edb7af3ba8a3af428b07a5425abc1399
spicyGrape <li.private0@gmail.com>
2023-04-04 08:52:51 +0800

    更改注释样式

--- dataProcesser.py
+++ dataProcesser.py
@@ -1,43 +1,61 @@
 #原始数据的数据量必须为偶数
-def SUCCESSIONAL_DIFFERENCE(variable_list):#对数列用逐差法求delta均值
+def SUCCESSIONAL_DIFFERENCE(variable_list):
+    """
+    对数列用逐差法求delta均值
+    """
     delta_list = TRANSFORM_TO_DELTA(variable_list)
     average= (1/delta_list[0])*SIGMA(delta_list)
     return average
 
-def SIGMA(variable_list):#求和数列
+def SIGMA(variable_list):
+    """
+    求和数列
+    """
     sum = 0
     for i in range(1,variable_list[0]+1):
         sum += variable_list[i]
     return sum
 
-def OPERATE_VARIABLE_LIST(variable_list,operation):#对数列每一位进行operation操作
+def OPERATE_VARIABLE_LIST(variable_list,operation):
+    """
+    对数列每一位进行operation操作
+    """
     operated_variable_list = [variable_list[0]]
     for i in range(1,variable_list[0]+1):
         operated_variable_list.append(operation(variable_list[i]))
     return operated_variable_list
 
 def GET_VARIABLE_LIST():
-    variable_list = []#0号存放变量个数，1~n存放变量
+    variable_list = []
     variable_list.append(int(input('Type in the number of variables: ')))
     for i in range(1,variable_list[0]+1):
         variable_list.append(float(input("Input the variables: ")))
         i += 1
     return variable_list
 
-def TRANSFORM_TO_DELTA(variable_list):#逐差的delta平均值
+def TRANSFORM_TO_DELTA(variable_list):
+    """
+    逐差的delta平均值
+    """
     transformed_list = [variable_list[0]//2]
     for i in range(1,variable_list[0]//2+1):
         transformed_list.append ((variable_list[i+variable_list[0]//2]-variable_list[i])/(variable_list[0]//2))
     return transformed_list
 
-def LIST_AVERAGE(variable_list):#传入需要求均值的数列
+def LIST_AVERAGE(variable_list):
+    """
+    传入需要求均值的数列
+    """
     sum = 0
     for i in range(1,variable_list[0]+1):
         sum += variable_list[i]
     average = sum / variable_list[0]
     return average
 
-def UNCERTAINTY(variable_list,ub):#传入未经处理的原始数据
+def UNCERTAINTY(variable_list,ub):
+    """
+    传入未经处理的原始数据
+    """
     delta_list = TRANSFORM_TO_DELTA(variable_list)
 
     average = SUCCESSIONAL_DIFFERENCE(variable_list)
